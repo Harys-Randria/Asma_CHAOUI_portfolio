ac2ea5febcbdd8dff49d4773fea705b923647de2
Harys Randrianomenjanahary <harysrandrianomenjanahary@gmail.com>
2026-03-13 12:44:19 +0300

    icône

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,19 +18,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/asma_chaoui.jpg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/asma_chaoui.jpg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/asma_chaoui.jpg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/asma_chaoui.jpg',
   },
 }
 
